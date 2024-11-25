1b1ecb2f0a614388e522bc3278dd25a005095ed7
Samuel Holdstock <samholdstock10@gmail.com>
2024-11-24 17:34:50 -1000

    add five pages

---
+++ final/project/subscription.js
@@ -0,0 +1,29 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const first = document.getElementById('first').value;
+    const last = document.getElementById('last').value;
+    const email = document.getElementById('email').value;
+    if (!first || !last) {
+        alert("You need a first and last name.")
+        return;
+    }
+    const formData = {
+        firstName: first,
+        lastName: last,
+        email: email
+    };
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "processing.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = xhr.response = JSON.parse(xhr.response);
+            document.getElementById('message').innerHTML = response.message;
+            document.getElementById('myForm').innerHTML = "";
+        } else if (xhr.readyState === 4) {
+            alert('Error submitting form.')
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+    console.log(formData)
+})
